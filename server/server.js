30d48e02a6b3763be358003eebed24c3174e6832
unknown <miha.drofenik92@gmail.com>
2018-10-23 01:14:04 +0200

    Added GET/todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var bodyParser = require("body-parser")
+const {ObjectID} = require("mongodb")
 
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo")
@@ -27,6 +28,22 @@ app.get("/todos", (req, res) => {
         res.status(400).send(e)
     })
 })
+
+app.get("/todos/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send()
+    }
+    User.findById(req.params.id)
+    .then(user => {
+        if (!user) {
+            return res.status(404).send("ni usera, id je prou") 
+        }
+        res.send({user})
+    })
+    .catch(e => {res.status(400).send()})
+})
+//WRONG ID 5bcd0a2e94f34532a0ca3563
+//CORECT ID 5bcc8a12d79474249cffc0bb
 
 app.listen(3000, () => {
     console.log("--------Started on port 3000--------")
