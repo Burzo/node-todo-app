b1cadaaec5085cf7b8460b58fde60aea6a57e389
unknown <miha.drofenik92@gmail.com>
2018-10-23 03:08:45 +0200

    fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://<Burzo>:<Fyhthk2eb13245!>@ds139193.mlab.com:39193/node-todo-api",{ useNewUrlParser: true })
+mongoose.connect("mongodb://burzo:mihili123@ds139193.mlab.com:39193/node-todo-api",{ useNewUrlParser: true })
 
 module.exports = {mongoose};
