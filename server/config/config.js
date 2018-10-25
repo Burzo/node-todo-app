b173bdb130cd5e1c8b2e34bbaf3f2db3ba624c44
unknown <miha.drofenik92@gmail.com>
2018-10-25 05:08:49 +0200

    added global variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
+
+
 var env = process.env.NODE_ENV || "development"
 
-if (env === "development") {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://burzo:mihili123@ds139193.mlab.com:39193/node-todo-api"
-} else if (env === "test") {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://burzo:mihili123@ds139193.mlab.com:39193/node-todo-api-test"
-} else if (env === "production") {
-    process.env.MONGODB_URI = "mongodb://burzo:mihili123@ds139193.mlab.com:39193/node-todo-api"
+if (env === "development" || env === "test") {
+    var config = require("./config.json")
+    var envConfig = config[env]
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    })
 }
